475c762fdb9f1f4c2b3afa944ffb9a8949f38fcf
jeembour <66140193+jeembour@users.noreply.github.com>
2021-09-18 23:31:13 +0200

    find an element and send a key

--- login.py
+++ login.py
@@ -1,13 +1,19 @@
 import unittest
 
+import requests as requests
 from selenium import webdriver
 
 
 class LogInTest(unittest.TestCase):
-    def login(self):
-        browser = webdriver.Chrome(executable_path="browser/chromedriver.exe")
-        browser.get("https:")
+    def test_valid_login(self):
+  #      user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
+ #       header =  {'user-agent': user_agent}
 
+        browser = webdriver.Chrome(executable_path='browser/chromedriver.exe')
+        browser.get('http://hrm-online.portnov.com/symfony/web/index.php/auth/login')
+        browser.find_element_by_xpath('//*[@id="txtUsername"]').send_keys('admin')
+
+        self.assertEqual(True, False)
 
 if __name__ == '__main__':
     unittest.main()
--- reserve.py
+++ reserve.py
@@ -3,14 +3,14 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        input = "Let us make it now or never"
+        input_string = "Let us make it now or never"
         expected_result = "never or now it make us Let"
 
-        output = self.reserve_string(input)
+        output = self.reserve_string(input_string)
         self.assertEqual(expected_result, output)  # add assertion here
 
-    def reserve_string(self, input: str):
-        return " ".join(list(reversed(input.split())))
+    def reserve_string(self, input_string: str):
+        return " ".join(list(reversed(input_string.split())))
 
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+     unittest.main()
