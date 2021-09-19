90507829ed9045a1ab5d0960fd907280dc68343d
jeembour <66140193+jeembour@users.noreply.github.com>
2021-09-19 21:32:28 +0200

    setUp and tearDown added

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 import requests as requests
@@ -6,16 +7,25 @@ from selenium import webdriver
 
 
 class LogInTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Chrome(executable_path='browser/chromedriver.exe')
+        self.browser.get('http://hrm-online.portnov.com/symfony/web/index.php/auth/login')
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+
     def test_valid_login(self):
   #      user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0"
  #       header =  {'user-agent': user_agent}
-
-        browser = webdriver.Chrome(executable_path='browser/chromedriver.exe')
-        browser.get('http://hrm-online.portnov.com/symfony/web/index.php/auth/login')
+        browser = self.browser
         browser.find_element_by_xpath('//*[@id="txtUsername"]').send_keys('admin')
         browser.find_element_by_id('txtPassword').send_keys('password')
         browser.find_element_by_css_selector('#btnLogin').click()
-        self.assertEqual(True, False)
+        time.sleep(3)
+        url = browser.current_url
+        welcome_message = browser.find_element_by_xpath('//*[@id="welcome"]').text
+        self.assertTrue(url.endswith('viewEmployeeList'))
+        self.assertEqual('Welcome Admin', welcome_message)
 
 if __name__ == '__main__':
     unittest.main()
